1623f6fd9b96360b56e0264795c7ee91a9d535f0
Mathieu Lecarme <mathieu@garambrogne.net>
2023-04-25 16:51:47 +0200

    HTTP/2 is great.

--- contrib/demo.py
+++ contrib/demo.py
@@ -22,10 +22,11 @@ if __name__ == '__main__':
     from pprint import pprint
     
     r = Readability('http://localhost:8000')
-    resp = httpx.get('https://linuxfr.org/news/ssl-rachete-harrison-consoles-bonne-nouvelle-pour-ardour')
-    assert resp.status_code == 200
+    with httpx.Client(http1=True, http2=True) as client:
+        resp = httpx.get('https://linuxfr.org/news/ssl-rachete-harrison-consoles-bonne-nouvelle-pour-ardour')
+        assert resp.status_code == 200, f'Status code should not be {resp.status_code}'
     rr = r.readability(resp.text)
     pprint(rr)
-    with open('/tmp/readability.html', 'w') as f:
+    with open('/tmp/readability.html', 'w', encoding='utf-8') as f:
         f.write(rr['content'])
 
